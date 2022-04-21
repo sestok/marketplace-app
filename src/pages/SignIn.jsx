9d4f538713f532a34e2258e56b1cb5aa2416f3e0
Sami Alhamad <sakosalo@outlook.com>
2022-04-21 06:15:16 +0300

    SignIn authentication with FirebaseAuth

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -3,6 +3,7 @@ import { useState } from 'react'
 import { Link, useNavigate } from 'react-router-dom'
 import { ReactComponent as ArrowRightIcon } from '../assets/svg/keyboardArrowRightIcon.svg'
 import visibilityIcon from '../assets/svg/visibilityIcon.svg'
+import { getAuth, signInWithEmailAndPassword } from 'firebase/auth'
 
 function SignIn() {
   const [showPassword, setShowPassword] = useState(false)
@@ -20,6 +21,23 @@ function SignIn() {
     }))
   }
 
+  const onSubmit = async (e) => {
+    e.preventDefault(e)
+    try {
+      const auth = getAuth()
+      const userCredential = await signInWithEmailAndPassword(
+        auth,
+        email,
+        password
+      )
+      if (userCredential.user) {
+        alert('Signed In!')
+      }
+    } catch (error) {
+      alert(error)
+    }
+  }
+
   return (
     <>
       <div className='pageContainer'>
@@ -27,7 +45,7 @@ function SignIn() {
           <p className='pageHeader'>Welcome!</p>
         </header>
 
-        <form>
+        <form onSubmit={onSubmit}>
           <input
             type='email'
             className='emailInput'
