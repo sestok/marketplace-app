12b0feb6a887f92c27f6e1bdc54263a9650b711b
Sami Alhamad <sakosalo@outlook.com>
2022-04-24 22:13:33 +0300

    passing the userRef

--- src/pages/NewListing.jsx
+++ src/pages/NewListing.jsx
@@ -1,9 +1,42 @@
 import React from 'react'
+import { useState, useEffect, useRef } from 'react'
+import { getAuth, onAuthStateChanged } from 'firebase/auth'
+import { useNavigate } from 'react-router-dom'
+import Spinner from '../components/Spinner'
 
 function NewListing() {
-  return (
-    <div>NewListing</div>
-  )
+  const [formData, setFormData] = useState({
+    type: 'sale',
+    name: '',
+    seats: '4',
+    wheels: '13',
+    autoParking: false,
+    address: '',
+    images: {},
+    latitude: '',
+    longitude: '',
+    offer: false,
+    regularPrice: 0,
+    discountedPrice: 0,
+  })
+  const auth = getAuth()
+  const navigate = useNavigate()
+  const isMounted = useRef(true)
+  useEffect(() => {
+    if (isMounted) {
+      onAuthStateChanged(auth, (user) => {
+        if (user) {
+          setFormData({ ...formData, userRef: user.uid })
+        } else {
+          navigate('/sign-in')
+        }
+      })
+    }
+    return () => {
+      isMounted.current = false
+    }
+  }, [isMounted])
+  return <div>NewListing</div>
 }
 
 export default NewListing
