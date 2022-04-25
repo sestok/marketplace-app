19a4f2207a28e44412198b0e65af8a01a30d474e
Sami Alhamad <sakosalo@outlook.com>
2022-04-25 22:59:33 +0300

    2

--- src/pages/NewListing.jsx
+++ src/pages/NewListing.jsx
@@ -87,6 +87,18 @@ function NewListing() {
               Rent
             </button>
           </div>
+          <label className='formLabel'>Name</label>
+          <input
+            className='formInputName'
+            type='text'
+            id='name'
+            value={name}
+            onChange={onMutate}
+            maxLength='32'
+            minLength='7'
+            required
+          />
+          
         </form>
       </main>
     </div>
