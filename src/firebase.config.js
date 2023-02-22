3f180e4ea8a2cabb10a3da7182c4cd4c48c6045c
Sami <98410836+sestok@users.noreply.github.com>
2023-02-22 18:59:53 +0300

    Update firebase.config.js

--- src/firebase.config.js
+++ src/firebase.config.js
@@ -6,7 +6,7 @@ const firebaseConfig = {
   authDomain: 'marketplace-app-47034.firebaseapp.com',
   projectId: 'marketplace-app-47034',
   storageBucket: 'marketplace-app-47034.appspot.com',
-  messagingSenderId: '',
+  messagingSenderId: '$GITHUB_secret_to_use',
   appId: '',
 }
 
